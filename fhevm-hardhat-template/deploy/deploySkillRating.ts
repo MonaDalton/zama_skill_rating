fe3b65d0c787e413c5cde158fd9c7334848830d0
MonaDalton <197275736+MonaDalton@users.noreply.github.com>
2025-11-13 14:51:09 +0800

    Improve deployment script with better documentation

--- fhevm-hardhat-template/deploy/deploySkillRating.ts
+++ fhevm-hardhat-template/deploy/deploySkillRating.ts
@@ -1,6 +1,8 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+// Deployment script for SkillRating contract
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
